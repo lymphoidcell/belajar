4c396fc51ce6916c74db015c61834a98a77b3849
Phanie <theophanie.tanzil@student.i3l.ac.id>
2024-05-14 05:30:46 +0700

    what i think about you

--- next-js-belajar/cobain/src/pages/about.tsx
+++ next-js-belajar/cobain/src/pages/about.tsx
@@ -2,17 +2,7 @@ const AboutPage = () => {
   return (
     <div>
       <h1>About Page</h1>
-      <p>Something about me that have changed ever since I met you.
-        I know things are not ideal between us, and sometimes it led me to think that I should just give up.
-        I hold on to what I have, and what I can get now, and I am grateful for that.
-        I thought that a little bit of waiting won't hurt. I thought that I can be patient enough to wait for you.
-        I cried days and nights by the thought of I don't like being treated like this.
-        Even so, I had a chance to leave, and to experience so much better with someone else, I chose to wait.
-        I felt hurt about everything you did and you might not realized it.
-        I felt like I am not enough for you, and I never will. And your actions toward me speak for how I feel.
-        Just so you know, whenever I choose to leave, I will always be waiting for you.
-        But for the sake of my well-being. I will give up one day about waiting for you.
-      </p>
+      <p>Fin.</p>
     </div>
   );
 };
