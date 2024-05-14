7951e67d1470bff90dc1f13a8614845448200d4a
Phanie <theophanie.tanzil@student.i3l.ac.id>
2024-05-14 11:33:33 +0700

    styling sesuatu

--- next-js-belajar/cobain/src/pages/index.tsx
+++ next-js-belajar/cobain/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Navbar from "@/components/layouts/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <div>
+        <Navbar />
         <div> Hello John </div>
       </div>
     </>
