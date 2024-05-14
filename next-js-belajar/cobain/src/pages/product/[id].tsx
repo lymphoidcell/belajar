d0b87079ae3c216209a1470d4bfd4fbb689b9fd8
Phanie <theophanie.tanzil@student.i3l.ac.id>
2024-05-14 11:16:37 +0700

    dynamic routing tapi kenapa ini aneh

--- next-js-belajar/cobain/src/pages/product/[id].tsx
+++ next-js-belajar/cobain/src/pages/product/[id].tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 
 const DetailProductPage = () => {
     const router = useRouter();
-    console.log(router);
+    console.log(router); // kok ga keliatan consolenya?
     return (
         <div>
             <h1>Detail Product</h1>
