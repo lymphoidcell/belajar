5563577c155ea9dfa7f96d58ef6fc2f66f436b48
Phanie <theophanie.tanzil@student.i3l.ac.id>
2024-05-14 11:36:15 +0700

    styling sesuatu

--- next-js-belajar/cobain/src/pages/_app.tsx
+++ next-js-belajar/cobain/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // fungsinya sama dengan main.jsx di react
 
-// import '@/styles/globals.css'
+import '@/styles/globals.css'
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
