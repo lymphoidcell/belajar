3858bba866ad73ce3964d259e89ec1efb0b8060c
Phanie <theophanie.tanzil@student.i3l.ac.id>
2024-05-16 06:45:19 +0700

    AppShell buat styling keseluruhan

--- next-js-belajar/cobain/src/pages/_app.tsx
+++ next-js-belajar/cobain/src/pages/_app.tsx
@@ -2,13 +2,12 @@
 
 import '@/styles/globals.css'
 import type { AppProps } from "next/app";
-import Navbar from "../components/layouts/Navbar";
+import AppShell from '@/components/layouts/AppShell';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <Navbar />
+    <AppShell>
       <Component {...pageProps} />
-    </div>
+    </AppShell>
   );
 }
